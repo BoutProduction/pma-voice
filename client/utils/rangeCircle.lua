3ec28e462028f986bb725764e5637763658dabf0
HoodRusher <boutlpyt@gmail.com>
2023-03-17 04:30:23 +0100

    Update rangeCircle.lua

--- client/utils/rangeCircle.lua
+++ client/utils/rangeCircle.lua
@@ -1,23 +1,23 @@
 AddEventHandler('pma-voice:setTalkingMode', function(mode)
-    local distance = Cfg.voiceModes[mode][1]
-    DrawVoiceDistanceMarker(distance, Cfg.voiceModes[mode][3], 150)
-    ShowNotification(string.format('New Range: ~h~%s', distance .. 'm'))
+    local distance = Cfg.voiceModes[mode]
+    DrawVoiceDistanceMarker(distance[1], distance[3], 150)
+    ShowNotification(string.format('New Range: ~h~%s', distance[2] .. ' ('  .. distance[1]*2 .. 'm)')
 end)
 
 
 function DrawVoiceDistanceMarker(distance, color, alpha)
     local r, g, b = color[1], color[2], color[3]
-    local pedCoords = GetEntityCoords(PlayerPedId())
+    local pCoords = GetEntityCoords(PlayerPedId())
 
     for i = alpha, 0, -5 do
       local a = math.floor((i * alpha) / 150)
-        DrawMarker(25, pedCoords.x, pedCoords.y, pedCoords.z - 0.825, 0, 0, 0, 0, 0, 0, distance, distance, 0.25, r, g, b, a, false, true, 2, nil, nil, false)
+        DrawMarker(25, pCoords.x, pCoords.y, pCoords.z - 0.825, 0, 0, 0, 0, 0, 0, distance, distance, 0.25, r, g, b, a, false, true, 2, nil, nil, false)
         Citizen.Wait(750 / alpha)
     end
 end
 
 function ShowNotification(text)
-	SetNotificationTextEntry('STRING')
+    SetNotificationTextEntry('STRING')
     AddTextComponentString(text)
-	DrawNotification(false, true)
+    DrawNotification(false, true)
 end
