065c5ef165a55ceac0fa0ce384108c3f2f7c2769
HoodRusher <boutlpyt@gmail.com>
2023-03-17 04:47:01 +0100

    Update rangeCircle.lua

--- client/utils/rangeCircle.lua
+++ client/utils/rangeCircle.lua
@@ -21,3 +21,28 @@ function ShowNotification(text)
     AddTextComponentString(text)
     DrawNotification(false, true)
 end
+
+
+CreateThread(function()
+    local state = Cfg.displayOnTalk
+    while Wait(1) do
+        if not state[1] then break
+        else
+            local pPed = PlayerPedId()
+            local pPos = GetEntityCoords(pPed)
+            local isTalking = NetworkIsPlayerTalking(PlayerId())
+
+            if isTalking then
+                for _, v in ipairs(GetActivePlayers()) do
+                    if v ~= PlayerId() then
+                        local tPed = GetPlayerPed(v)
+                        local tPos = GetEntityCoords(tPed)
+                        if Vdist(pPos, tPos) < state[2] then
+                            DrawMarker(25, tPos.x, tPos.y, tPos.z - 0.950, 0, 0, 0, 0, 0, 0, 0.75, 0.75, 0.0, 52, 204, 255, 55, false, true, 2, false, nil, nil, false)
+                        end
+                    end
+                end
+            end
+        end
+    end
+end)
